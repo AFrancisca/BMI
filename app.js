1998395936e32b40f9953c8ac5591406274dea3f
AFrancisca <100122108+AFrancisca@users.noreply.github.com>
2023-06-13 09:15:39 +0200

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
-const calculateButtonMark = document.querySelector(".Mark button");
+// Define the compareBMI function
+//parsefloat to change strings into digits
+const compareBMI = () => {
+  let BMImark = parseFloat(document.getElementById("resultMark").value);
+  let BMIjohn = parseFloat(document.getElementById("resultJohn").value);
+
+  let comparisonText;  
+  if (BMImark > BMIjohn) {
+    comparisonText = "Mark's BMI is greater than John's BMI";
+  } else if (BMImark < BMIjohn) {
+    comparisonText = "John's BMI is greater than Mark's BMI";
+  } else {
+    comparisonText = "Mark's BMI is the same as John's BMI";
+  }
+
+  document.getElementById("answer").textContent = comparisonText;
+}
+
+
+// Define the calculateBMI function
+const calculateBMI = (person) => {
+  let mass = document.getElementById(`m${person}`).value;
+  let height = document.getElementById(`h${person}`).value;
+  let bmi = (mass / (height * height)).toFixed(1);//to add only one number after the decimal point
+  document.getElementById(`result${person}`).value = bmi;
+}
+
+// Attach event listeners after function
+const calculateButtonMark = document.querySelector(".Mark");
 calculateButtonMark.addEventListener("click", function() {
   calculateBMI("Mark");
 });
 
-const calculateButtonJohn = document.querySelector(".John button");
+const calculateButtonJohn = document.querySelector(".John");
 calculateButtonJohn.addEventListener("click", function() {
   calculateBMI("John");
 });
@@ -11,29 +39,8 @@ calculateButtonJohn.addEventListener("click", function() {
 const compareButton = document.getElementById("compare");
 compareButton.addEventListener("click", compareBMI);
 
-//calculating BMI of each person
-function calculateBMI(person) {
-  var m = document.getElementById(`m${person}`).value;
-  var h = document.getElementById(`h${person}`).value;
-  var bmi = (m / (h * h)).toFixed(1);
-  document.getElementById(`result${person}`).value = bmi;
-}
-//compare bmis
-//parsefloat to convert strings to digits
-function compareBMI() {
-  var BMImark = parseFloat(document.getElementById("resultMark").value);
-  var BMIjohn = parseFloat(document.getElementById("resultJohn").value);
 
-  var comparisonText = "Mark's BMI is the same as John's BMI";
-  if (BMImark > BMIjohn) {
-    comparisonText = "Mark's BMI is greater than John's BMI";
-  } else if (BMImark < BMIjohn) {
-    comparisonText = "John's BMI is greater than Mark's BMI";
-  }
 
-  document.getElementById("answer").textContent = comparisonText;
-}
 
-  
 
   
